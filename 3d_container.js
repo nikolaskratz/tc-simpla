c03f85919b1cd54e6aafa1721ffe91f31740ae5e
nikolaskratz <nikolas.kratz@gmail.com>
2021-01-13 11:54:55 +0100

    multiple elements, input fields, exact destination calculation

--- 3d_container.js
+++ 3d_container.js
@@ -1,12 +1,61 @@
+//container dimensions
+var container_width = 100
+var container_height = 50
+var container_depth = 50
+
+//input for  dummy elements and algorithm output (Dube et. al)
+elementPos = [
+    {x: -70, y: 50, z: 0},
+    {x: -70, y: 50, z: 0},
+    {x: -70, y: 50, z: 0},
+    {x: -70, y: 50, z: 0},
+    {x: -30, y: 50, z: 0},
+    {x: 30, y: 50, z: 0},
+    {x: 80, y: 50, z: 0},
+    {x: 120, y: 50, z: 0}
+]
+
+elementDim = [
+    {width: 15, height: 15, depth: 15},
+    {width: 15, height: 15, depth: 15},
+    {width: 15, height: 15, depth: 15},
+    {width: 15, height: 15, depth: 15},
+    {width: 40, height: 30, depth: 10},
+    {width: 40, height: 30, depth: 10},
+    {width: 35, height: 25, depth: 35},
+    {width: 20, height: 40, depth: 60}
+]
+
+elementDest = [
+    {x: 0, y: 0, z: 0},
+    {x: 15, y: 0, z: 0},
+    {x: 30, y: 0, z: 0},
+    {x: 45, y: 0, z: 0},
+    {x: 60, y: 0, z: 0},
+    {x: 0, y: 15, z: 0},
+    {x: 0, y: 0, z: 15},
+    {x: 35, y: 0, z: 15}
+]
+
+elememntColor = [0xFFCC00, 0xff0000, 0xff8000, 0x808080, 0x00ff00, 0x00ffff, 0x0040ff, 0xff00ff]
+
+//adapt destination output to match 3d model coordinate system
+elementDest.forEach((dest, index) => {
+    dest.x = dest.x - container_width/2 + elementDim[index].width/2
+    dest.y = dest.y - container_height/2 + elementDim[index].height/2
+    dest.z = dest.z - container_depth/2 + elementDim[index].depth/2 
+})
+
 let camera, controls, scene, renderer;
 
-init();
-// createElements()
-animate();
+init()
+animate()
 
 //public vars
 var movement = false
 var elements = []
+
+
 
 //element class
 class Element {
@@ -22,14 +71,32 @@ class Element {
         this.velocity = calcVelocity(width, height, depth, posX, posY, posZ, destination)
         this.hit = hit
     }
+
 }
 
-var el = new Element(15,15,15,-100,50,0, {x:0, y:0, z:0}, {x:0, y:0, z:0}, 0xFFCC00, false)
-elements.push(el)
-// elements[0].velocity = calcVelocity(elements[0])
+createElements()
+
+function createElements() {
+    for(var i=0; i<elementDim.length; i++){
+        let el = new Element(
+            elementDim[i].width, elementDim[i].height, elementDim[i].depth, 
+            elementPos[i].x, elementPos[i].y, elementPos[i].z,
+            {x: elementDest[i].x, y: elementDest[i].y, z: elementDest[i].z},
+            {x:0, y:0, z:0}, 
+            elememntColor[i], 
+            false)
+        elements.push(el)
+    }
+}
+
+// var el2 = new Element(1,30,1,0,0,0, {x:0, y:0, z:0}, {x:0, y:0, z:0}, 0x000000, false)
+// var el3 = new Element(30,1,1,0,0,0, {x:0, y:0, z:0}, {x:0, y:0, z:0}, 0x000000, false)
+// var el4 = new Element(1,1,30,0,0,0, {x:0, y:0, z:0}, {x:0, y:0, z:0}, 0x000000, false)
 
 function calcVelocity(width, height, depth, posX, posY, posZ, destination) {
     var velocity = {x:0, y:0, z:0}
+
+    //recalculate posX, posY, posZ to match (left-lower-behind point?) of box??
 
     //assumption: elements are always above container (y>0), zContainer=zElement, x can be both < and >0
 
@@ -63,10 +130,8 @@ function calcVelocity(width, height, depth, posX, posY, posZ, destination) {
     velocity.y = yMove/total
     velocity.z = zMove/total
 
-    console.log(velocity.z)
     return velocity
 }
-
 
 function init() {
 
@@ -80,7 +145,7 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.set(0, 1.5, 3);
+    camera.position.set(0, 50, 100);
 
     // controls
 
@@ -90,21 +155,19 @@ function init() {
 
     controls.screenSpacePanning = false;
 
-    controls.minDistance = 100;
-    controls.maxDistance = 500;
+    controls.minDistance = 70;
+    controls.maxDistance = 1000;
 
     controls.maxPolarAngle = Math.PI / 2;
 
     //container
-    var geometry = new THREE.BoxGeometry(100, 50, 50)
+    var geometry = new THREE.BoxGeometry(container_width, container_height, container_depth)
     var material = new THREE.MeshLambertMaterial({ color: 0x111111 })
     var mesh = new THREE.Mesh(geometry, material)
     mesh.position.set(0, 0, 0)
     scene.add(mesh)
     material.blending = THREE.AdditiveBlending
     material.transparent = true
-
-    // }
 
     // lights
 
@@ -131,27 +194,32 @@ function animate() {
 
     requestAnimationFrame(animate);
 
-    var speed = 5
+    var speed = 2
 
     if(movement) {
-        if(!elements[0].hit) {
-            console.log('x position: '+elements[0].mesh.position.x)
-            elements[0].mesh.position.x += elements[0].velocity.x*speed
-            elements[0].mesh.position.y -= elements[0].velocity.y*speed
-            elements[0].mesh.position.z -= elements[0].velocity.z*speed
-
-            //detect hit
-            let distanceY = elements[0].mesh.position.y - elements[0].destination.y
-            if(distanceY < (elements[0].velocity.y*speed)) {
-                while(distanceY < (elements[0].velocity.y*speed) && speed>0.001) {
-                    speed *= 0.5
-                    console.log('speed: '+speed)
+        elements.forEach((element, index) => {
+            if(index==0 || elements[index-1].hit) {
+                if(!element.hit) {
+                    console.log('x position: '+element.mesh.position.x)
+                    element.mesh.position.x += element.velocity.x*speed
+                    element.mesh.position.y -= element.velocity.y*speed
+                    element.mesh.position.z -= element.velocity.z*speed
+        
+                    //detect hit
+                    let distanceY = element.mesh.position.y - element.destination.y
+                    if(distanceY < (element.velocity.y*speed)) {
+                        while(distanceY < (element.velocity.y*speed) && speed>0.001) {
+                            speed *= 0.5
+                            console.log('speed: '+speed)
+                        }
+                    }
+                    if(distanceY < 0.01) {
+                        element.hit = true
+                    }
                 }
             }
-            if(distanceY < 0.1) {
-                elements[0].hit = true
-            }
-        }
+            
+        })
     }
 
     controls.update();
---
+++ landing_page.js
@@ -0,0 +1,5 @@
+const PackingBtn = document.querySelector('#start_packing')
+
+PackingBtn.addEventListener('click', () => {
+    window.open("simpla_index.html", "_self")
+})
