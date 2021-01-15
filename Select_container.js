b8f62b69828f983ed07d7cb655f870d052ded5c3
Nikolaus Pinger <Nikolaus.pinger@gmx.de>
2021-01-15 14:53:43 +0100

    Button fix

--- Select_container.js
+++ Select_container.js
@@ -1,5 +1,31 @@
+const AddBtn4 = document.querySelector('#Add4')
+
 const GoBtn = document.querySelector('#Go')
 
 GoBtn.addEventListener('click', () => {
-    window.open("3d_container.html", "_self")
+        window.open("3d_container.html", "_self")
+    
 })
+/*
+AddBtn4.addEventListener('click', () => {
+    addTolist("Palette", 1)
+})*/
+
+let Selected_items = {"Palette1": 1, "Box": 2};
+
+function addTolist(item, quantity){
+    Selected_items.push({
+        key: item,
+        value: quantity,
+    })
+}
+
+function printList(){
+    var Selected_items = {"Palette1": 1, "Box": 2};
+    console.log(Selected_items);
+    
+    
+}
+
+/*document.querySelector('.results').innerHTML = 'Hello World!';
+*/
