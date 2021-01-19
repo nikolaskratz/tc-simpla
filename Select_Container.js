bebbdd53fb766dd7bbe453fca6ddfca11ec02f92
Nikolaus Pinger <Nikolaus.pinger@gmx.de>
2021-01-19 12:53:56 +0100

    add Select_Container

---
+++ Select_Container.js
@@ -0,0 +1,44 @@
+const AddBtn4 = document.querySelector('#Add4')
+
+const GoBtn = document.querySelector('#Go')
+
+GoBtn.addEventListener('click', () => {
+        window.open("Select_Box.html", "_self")
+    
+})
+/*
+AddBtn4.addEventListener('click', () => {
+    addTolist("Palette", 1)
+})*/
+
+let Selected_items = {"Palette1": 1, "Box": 2};
+
+function addTolist(item, quantity){
+    Selected_items.push({
+        key: item,
+        value: quantity,
+    })
+}
+
+document.querySelector('#Add4').addEventListener('click', printList);
+    function printList(){
+        var Selected_items = {"Palette1": 1, "Box": 2};
+        document.querySelector("output2").textContent = Selected_items ;
+        
+        
+    }
+
+
+document.querySelector('#berechnen').addEventListener('click', berechne_Volumen);
+ 
+  function berechne_Volumen () {
+	var Height = parseInt(document.getElementById("Height").value),
+        Width = parseInt(document.getElementById("Width").value),
+        Depth = parseInt(document.getElementById("Depth").value),
+	    Volume = Math.round(Height*Width*Depth);
+				
+	document.querySelector("output").textContent = Volume ;
+  }
+
+/*document.querySelector('.results').innerHTML = 'Hello World!';
+*/
