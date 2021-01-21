d7958e04a48ffbd26be91b8f261b1f3a25c23d99
Nikolaus Pinger <Nikolaus.pinger@gmx.de>
2021-01-21 21:35:28 +0100

    button

--- Start.js
+++ Start.js
@@ -1,5 +1,11 @@
 const PackingBtn = document.querySelector('#start_packing')
+const UploadBtn = document.querySelector('#Upload')
+
 
 PackingBtn.addEventListener('click', () => {
     window.open("Select_Container.html", "_self")
+})
+
+UploadBtn.addEventListener('click', () => {
+    window.open("3d_container.html", "_self")
 })
