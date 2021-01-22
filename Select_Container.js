def8ffa58130a9df06067be09196dc30ae9cda72
Nikolaus Pinger <Nikolaus.pinger@gmx.de>
2021-01-22 12:41:18 +0100

    change Start Background

--- Select_Container.js
+++ Select_Container.js
@@ -1,38 +1,14 @@
 const AddBtn4 = document.querySelector('#Add4')
-
 const GoBtn = document.querySelector('#Go')
 
 GoBtn.addEventListener('click', () => {
         window.open("Select_Box.html", "_self")
     
 })
-/*
-AddBtn4.addEventListener('click', () => {
-    addTolist("Palette", 1)
-})*/
-
-let Selected_items = {"Palette1": 1, "Box": 2};
-
-function addToDict(item, quantity){
-    Selected_items.push({
-        key: item,
-        value: quantity,
-    })
-}
-
 
 document.querySelector('#Add4').addEventListener('click', addToList);
-    function printList(){
-        var list= []
-        var Selected_items = {
-            "Palette1": 1, 
-            "Box": 2
-        };
-
-        document.querySelector("output2").textContent(Selected_items) ;
+    
         
-        
-    }
 
 
 document.querySelector('#berechnen').addEventListener('click', berechne_Volumen);
